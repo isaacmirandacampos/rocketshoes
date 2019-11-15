3644775c7b093ef26944fccff1bb1793890e855d
isaacmirandacampos <isaacdmcampos@outlook.com>
2019-11-15 20:37:28 -0300

    refactoring the code

--- src/pages/Cart/index.js
+++ src/pages/Cart/index.js
@@ -1,14 +1,16 @@
 import React from 'react';
 import { connect } from 'react-redux';
+import { bindActionCreators } from 'redux';
 import {
   MdRemoveCircleOutline,
   MdAddCircleOutline,
   MdDelete,
 } from 'react-icons/md';
+import * as CartActions from '../../store/modules/cart/actions';
 
 import { Container, ProductTable, Total } from './styles';
 
-function Cart({ cart, dispatch }) {
+function Cart({ cart, removeFromCart }) {
   return (
     <Container>
       <ProductTable>
@@ -46,14 +48,11 @@ function Cart({ cart, dispatch }) {
                 <strong>R$258,80</strong>
               </td>
               <td>
-                <button type="button">
-                  <MdDelete
-                    size={20}
-                    color="#7159c1"
-                    onClick={() =>
-                      dispatch({ type: 'REMOVE_FROM_CART', id: product.id })
-                    }
-                  />
+                <button
+                  type="button"
+                  onClick={() => removeFromCart(product.id)}
+                >
+                  <MdDelete size={20} color="#7159c1" />
                 </button>
               </td>
             </tr>
@@ -74,4 +73,7 @@ function Cart({ cart, dispatch }) {
 const mapStateToProps = state => ({
   cart: state.cart,
 });
-export default connect(mapStateToProps)(Cart);
+
+const mapDispatchToProps = dispatch =>
+  bindActionCreators(CartActions, dispatch);
+export default connect(mapStateToProps, mapDispatchToProps)(Cart);
